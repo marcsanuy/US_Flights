9bc28ee137481afd78e676465b2c1f6f7bf7fd7d
marcsanuy <marcsanuy@gmail.com>
2020-10-14 13:49:37 +0200

    Exercise 7

---
+++ Exercici 7.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-10-2020 a las 13:48:55
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `usairlineflights2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `flights`
+--
+
+CREATE TABLE `flights` (
+  `flightID` int(11) NOT NULL,
+  `colYear` smallint(6) DEFAULT NULL,
+  `colMonth` smallint(6) DEFAULT NULL,
+  `DayOfMonths` smallint(6) DEFAULT NULL,
+  `DayOfWeek` smallint(6) DEFAULT NULL,
+  `DepTime` smallint(6) DEFAULT NULL,
+  `CRSDepTime` smallint(6) DEFAULT NULL,
+  `ArrTime` smallint(6) DEFAULT NULL,
+  `CRSArrTime` smallint(6) DEFAULT NULL,
+  `UniqueCarrier` varchar(32) DEFAULT NULL,
+  `FlightNum` varchar(32) DEFAULT NULL,
+  `TailNum` varchar(32) DEFAULT NULL,
+  `ActualElapsedTime` smallint(6) DEFAULT NULL,
+  `CRSElapsedTime` smallint(6) DEFAULT NULL,
+  `AirTime` smallint(6) DEFAULT NULL,
+  `ArrDelay` smallint(6) DEFAULT NULL,
+  `DepDelay` smallint(6) DEFAULT NULL,
+  `Origin` varchar(32) DEFAULT NULL,
+  `Dest` varchar(32) DEFAULT NULL,
+  `Distance` smallint(6) DEFAULT NULL,
+  `TaxiIn` smallint(6) DEFAULT NULL,
+  `TaxiOut` smallint(6) DEFAULT NULL,
+  `Cancelled` tinyint(1) DEFAULT NULL,
+  `CancellationCode` varchar(32) DEFAULT NULL,
+  `Diverted` tinyint(1) DEFAULT NULL,
+  `CarrierDelay` smallint(6) DEFAULT NULL,
+  `WeatherDelay` smallint(6) DEFAULT NULL,
+  `NASDelay` smallint(6) DEFAULT NULL,
+  `SecurityDelay` smallint(6) DEFAULT NULL,
+  `LateAircraftDelay` smallint(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `flights`
+--
+
+INSERT INTO `flights` (`UniqueCarrier`, `AVG(ArrDelay)`) VALUES
+('TZ', '38.5714'),
+('F9', '32.8571'),
+('B6', '20.3611'),
+('PI', '17.7273'),
+('9E', '16.4000'),
+('YV', '13.2727'),
+('EV', '13.0725'),
+('OH', '12.9310'),
+('FL', '12.0435');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `flights`
+--
+ALTER TABLE `flights`
+  ADD PRIMARY KEY (`flightID`),
+  ADD KEY `Origin` (`Origin`),
+  ADD KEY `Dest` (`Dest`),
+  ADD KEY `UniqueCarrier` (`UniqueCarrier`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `flights`
+--
+ALTER TABLE `flights`
+  MODIFY `flightID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4759;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `flights`
+--
+ALTER TABLE `flights`
+  ADD CONSTRAINT `flights_ibfk_1` FOREIGN KEY (`Origin`) REFERENCES `usairports` (`IATA`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `flights_ibfk_2` FOREIGN KEY (`Dest`) REFERENCES `usairports` (`IATA`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `flights_ibfk_3` FOREIGN KEY (`UniqueCarrier`) REFERENCES `carriers` (`CarrierCode`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
